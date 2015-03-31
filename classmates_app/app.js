24ecf6cfef1a804224986f87612ea9ffcbfc4362
Dennis Charles Hackethal <dennis@ga.co>
2015-03-31 15:49:13 -0700

    Implement student CRUD

--- classmates_app/app.js
+++ classmates_app/app.js
@@ -10,19 +10,24 @@ var app = express();
 // Tell express to use EJS as its view engine
 app.set("view engine", "ejs");
 
+// Set up body parser
+app.use(bodyParser.urlencoded({ extended: false }));
+
+var classmates = [];
+
 // Set a root route
-
 app.get('/', function(req, res) {
-	var classmates = [
-		{ name: "John", age: "21" },
-		{ name: "Brett", age: "14"},
-		{ name: "Christina", age: "24"},
-		{ name: "Rick", age: "27"}
-	];
-
-	res.render("index", { taco: classmates } );
+	res.render("index", { classmates: classmates } );
 });
 
+app.post('/add', function(req, res) {
+	// req.params // request params
+	// req.query // query params
+	// req.body // body params
+
+	classmates.push(req.body);
+	res.redirect('/');
+});
 
 app.listen(3000, function() {
 	console.log('Server listening on port 3000');
